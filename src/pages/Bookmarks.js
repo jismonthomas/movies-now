37215e28e1f629ffab9f05c74376169127420806
jismonthomas <jismonthomas>
2022-01-12 17:47:12 -0800

    fixed scrolling issue on my-list page

--- src/pages/Bookmarks.js
+++ src/pages/Bookmarks.js
@@ -3,9 +3,13 @@ import { useSelector } from "react-redux";
 import Container from '../components/UI/Container';
 import MovieCard from "../components/MovieCard/MovieCard";
 import MovieWrappedRow from "../components/UI/MovieWrappedRow";
+import { useEffect } from "react";
 
 const Bookmarks = () => {
     const favouriteMovies = useSelector(state => state.movies.favouriteMovies);
+    useEffect(() => {
+        window.scroll(0, 0);
+    }, [])
     return (
         <Container>
             <h1 className="pt-5 mt-5">My List </h1>
